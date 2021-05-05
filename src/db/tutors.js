8b8b31400c9d5a7cca15d5565f2112e5cf8aa868
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-05 14:35:38 +0200

    defines Tutor model

---
+++ src/db/tutors.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Tutor = sequelize.define(
+    'tutor',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      expertise: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  return Tutor;
+};
